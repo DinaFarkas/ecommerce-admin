7866a2dd7d3d1390c20122eea9f04edea27f9335
DinaFarkas <dinafarkasns@gmail.com>
2024-06-24 19:42:11 +0200

    Pocetak projekta 11min

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    //bg=back-ground p je padding a px udaljenost teksta od boxa
+    <div className="bg-blue-900 w-screen h-screen flex items-center">
+      <div className="text-center w-full">
+        <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+      </div>
+    </div>
+  );
+}
