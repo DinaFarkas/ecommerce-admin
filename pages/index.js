646dd9d7820017537199a50e2fab937ad280ddee
DinaFarkas <dinafarkasns@gmail.com>
2024-06-27 17:39:25 +0200

    Ne radi port, namesten google id , 20:40

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
-  return (
-    //bg=back-ground p je padding a px udaljenost teksta od boxa
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+
+  const { data: session } = useSession()
+  if(!session) {
+
+      return (
+      //bg=back-ground p je padding a px udaljenost teksta od boxa
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button onClick={()=>signIn('google')} className="bg-white p-2 rounded-md">Login with Google</button>
+        </div>
       </div>
-    </div>
+    );
+  }
+  
+  return(
+    <div>logged in{session.user.email}</div>
   );
 }
+
